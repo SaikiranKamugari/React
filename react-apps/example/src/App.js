5eab3cebaa0266fb2972187b2a3b2ed627c37bd7
bhanupasupuleti <kamugarisk@gmail.com>
2024-03-26 21:29:20 -0400

    files added

--- react-apps/example/src/App.js
+++ react-apps/example/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useState } from 'react';
+
+const CounterComponent = () => {
+  const [count, setCount] = useState(50);
+
+  const increment = () => {
+    setCount(count + 50);
+  };
+
+  const decrement = () => {
+    setCount(count - 50);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    < div >
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
 
-export default App;
+    </div >
+  );
+
+};
+
+export default CounterComponent;
